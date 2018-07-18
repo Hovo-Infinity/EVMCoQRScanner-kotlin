2fe7c9d3ce5a8927c74dc22d17bf5780a6023b53
Hovhannes Stepanyan <hovhannesstepanyan@Hovhanness-Mac-mini.local>
2018-07-18 18:20:57 +0400

    display content

--- app/src/main/java/com/example/hovhannesstepanyan/qrscanner/Main2Activity.kt
+++ app/src/main/java/com/example/hovhannesstepanyan/qrscanner/Main2Activity.kt
@@ -1,28 +1,31 @@
 package com.example.hovhannesstepanyan.qrscanner
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
 import android.widget.TextView
 
 class Main2Activity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         val bundle = intent.extras
 
-        val value = bundle.getString("privet")
+        val value = bundle.get("privet") as HashMap<*, *>
 
         val layout: LinearLayout = findViewById(R.id.layout)
 
 
-
-        val textView = TextView(this)
-        textView.setText(value)
-
-        layout.addView(textView)
+        value.forEach {
+            val textView = TextView(this)
+            textView.text = "${it.key} = ${it.value}"
+            layout.addView(textView)
+        }
 
 
 
--- app/src/main/java/com/example/hovhannesstepanyan/qrscanner/MainActivity.kt
+++ app/src/main/java/com/example/hovhannesstepanyan/qrscanner/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.hovhannesstepanyan.qrscanner
 
 import android.app.Activity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.nfc.FormatException
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
 import com.mastercard.mpqr.pushpayment.exception.ConflictiveTagException
@@ -16,7 +14,6 @@ import com.mastercard.mpqr.pushpayment.model.PushPaymentData
 import com.mastercard.mpqr.pushpayment.parser.Parser
 import com.mastercard.mpqr.pushpayment.scan.PPIntentIntegrator
 import com.mastercard.mpqr.pushpayment.scan.constant.PPIntents
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,13 +26,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun parseQRCode(code: String) {
+    private fun parseQRCode(code: String): PushPaymentData {
         val qrcode: PushPaymentData
         try {
             qrcode = Parser.parseWithoutTagValidation(code)
             qrcode.validate()
-            Log.d("data", qrcode.dumpData())
-            System.out.println(qrcode.dumpData())
+            return qrcode
         } catch (e: ConflictiveTagException) {
             println("ConflictiveTagException : $e")
         } catch (e: InvalidTagValueException) {
@@ -47,6 +43,7 @@ class MainActivity : AppCompatActivity() {
         } catch (e: com.mastercard.mpqr.pushpayment.exception.FormatException) {
             println("FormatException : $e")
         }
+        return PushPaymentData()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -54,9 +51,38 @@ class MainActivity : AppCompatActivity() {
             PPIntentIntegrator.parseActivityResult(requestCode, resultCode, data)
             val qrCode = data?.getSerializableExtra(PPIntents.PUSH_PAYMENT_DATA) as? PushPaymentData
             if (qrCode != null) {
-//                Toast.makeText(this, qrCode.dumpData(), Toast.LENGTH_LONG).show()
+                val extra = hashMapOf<String, String>()
+                val methods = qrCode.javaClass.declaredMethods
+                for (method in methods) {
+                    if (method.name.startsWith("get") &&
+                            method.parameterTypes.count() == 0 &&
+                            Unit::class.java != method.returnType) {
+                        val value = method.invoke(qrCode)
+                        if (value != null) {
+                            val key = method.name.removePrefix("get")
+                            if (value is String || value is Double) {
+                                extra[key] = value.toString()
+                            } else {
+                                val subMethods = value.javaClass.declaredMethods
+                                val subValue = mutableMapOf<String, String>()
+                                for (subMethod in subMethods) {
+                                    if (subMethod.name.startsWith("get") &&
+                                            subMethod.parameterTypes.count() == 0 &&
+                                            Unit::class.java != subMethod.returnType) {
+                                        val subKey = subMethod.name.removePrefix("get")
+                                        val putValue = subMethod.invoke(value)
+                                        if (putValue != null) {
+                                            subValue[subKey] = putValue.toString()
+                                        }
+                                    }
+                                }
+                                extra[key] = subValue.toString()
+                            }
+                        }
+                    }
+                }
                 val intent = Intent(this, Main2Activity::class.java)
-                intent.putExtra("privet", qrCode.additionalData.mobileNumber)
+                intent.putExtra("privet", extra)
                 startActivity(intent)
             }
         } else if (resultCode == Activity.RESULT_CANCELED) {
